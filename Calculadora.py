d8275e94de189246ae7b8313e559205948fd4318
RamCodeZ3 <arammusset7@gmail.com>
2025-07-31 01:31:06 -0400

    ya la calculadora esta arreglada con buena practica siguiendo el estilo de codigo de pep8

--- Calculadora.py
+++ Calculadora.py
@@ -18,42 +18,51 @@ def calcular():
         resultado = eval(operacion)
         pantalla.delete(0, tk.END)
         pantalla.insert(tk.END, str(resultado))
-       
-        if re.findall(r'\.0$', pantalla.get()):
-           pantalla.delete(len(pantalla.get())- 2, tk.END)
-           
-    except: 
-        pantalla.delete(0, tk.END)
-        pantalla.insert(tk.END, str("ERROR"))
 
-def Raiz():
-    numeral = pantalla.get()
-    try:
-     resultado = math.sqrt(int(numeral))
-     pantalla.delete(0, tk.END)
-     pantalla.insert(tk.END, str(resultado))
-     if re.findall(r'.0$', pantalla.get()):
-         pantalla.delete(len(pantalla.get())- 2, tk.END)
-         
+        if re.findall(r'\.0$', pantalla.get()):
+            pantalla.delete(len(pantalla.get())-2, tk.END)
+
     except:
         pantalla.delete(0, tk.END)
         pantalla.insert(tk.END, str("ERROR"))
 
+
+def Raiz():
+    numeral = pantalla.get()
+    try:
+        resultado = math.sqrt(int(numeral))
+        pantalla.delete(0, tk.END)
+        pantalla.insert(tk.END, str(resultado))
+        if re.findall(r'.0$', pantalla.get()):
+            pantalla.delete(len(pantalla.get())-2, tk.END)
+
+    except:
+        pantalla.delete(0, tk.END)
+        pantalla.insert(tk.END, str("ERROR"))
+
+
 def insert_sign():
-   if re.findall(r'^\d', pantalla.get()) and not pantalla.get() == "0":
-     pantalla.insert(0, "-")
-   elif re.findall(r'^-', pantalla.get()):
-      pantalla.delete(0)
+    if re.findall(r'^\d', pantalla.get()) and not pantalla.get() == "0":
+        pantalla.insert(0, "-")
+    elif re.findall(r'^-', pantalla.get()):
+        pantalla.delete(0)
+
 
 def clear():
     pantalla.delete(0, tk.END)
     pantalla.insert(tk.END, "0")
 
-def clear2(): 
-   if len(pantalla.get()) == 1:
-      pantalla.delete(len(pantalla.get())-1, tk.END)
-      pantalla.insert(tk.END, "0")
-   else:  pantalla.delete(len(pantalla.get())-1, tk.END)
+
+def clear2():
+    if len(pantalla.get()) == 1:
+        pantalla.delete(len(pantalla.get())-1, tk.END)
+        pantalla.insert(tk.END, "0")
+
+    else:
+        pantalla.delete(
+            len(pantalla.get()) - 1,
+            tk.END
+        )
 
 
 app = tk.Tk()
@@ -62,7 +71,15 @@ app.configure(background="#212f3c")
 app.title("Calculadora")
 app.resizable(height=False, width=False)
 
-pantalla = tk.Entry(app, width=50, bg="#154360", border=0, fg="White", font=("Sans", 22))
+pantalla = tk.Entry(
+    app,
+    width=50,
+    bg="#154360",
+    border=0,
+    fg="White",
+    font=("Sans", 22)
+)
+
 pantalla.grid(row=0, column=0, columnspan=4,)
 pantalla.insert(tk.END, "0")
 
@@ -101,19 +118,24 @@ for numero in numeros:
        fg="white",
        command=lambda num=numero: clic(num)
     )
-    
-    boton_numerico.grid(row=fila, column=columna, padx=0, pady=2)
+
+    boton_numerico.grid(
+        row=fila,
+        column=columna,
+        padx=0,
+        pady=2
+    )
     columna += 1
 
     if columna > 3:
         columna = 0
-        fila += 1 
+        fila += 1
 
     elif re.findall(r'\d', numero):
-       boton_numerico.configure(bg="#34495e")
+        boton_numerico.configure(bg="#34495e")
 
     elif re.findall(r'\.', numero):
-       boton_numerico.configure(bg="#34495e")
+        boton_numerico.configure(bg="#34495e")
 
 app.grid_columnconfigure(0, weight=1)
 app.grid_columnconfigure(1, weight=1)
@@ -126,13 +148,13 @@ Result = tk.Button(
    border=0,
    fg="white",
    font=("Sans", 16),
-   command= calcular,
+   command=calcular,
    bg="#3EEC30"
 )
 Result.grid()
 Result.place(x=82, y=375)
 
-Delete= tk.Button(
+Delete = tk.Button(
    text="CE",
    width=6,
    height=1,
@@ -140,7 +162,7 @@ Delete= tk.Button(
    fg="white",
    border=0,
    font=("Sans", 16),
-   command= clear
+   command=clear
    )
 
 Delete.grid()
@@ -160,15 +182,16 @@ Delete2 = tk.Button(
 
 Delete2.grid(row=1, column=0)
 
-Sqrt = tk.Button(app,
-                 text="√2",
-                 height=3,
-                 width=8,
-                 border=0,
-                 font=("Sans", 12),
-                 bg="#31404F",
-                 fg="white",
-                 command=Raiz
+Sqrt = tk.Button(
+   app,
+   text="√2",
+   height=3,
+   width=8,
+   border=0,
+   font=("Sans", 12),
+   bg="#31404F",
+   fg="white",
+   command=Raiz
 )
 
 Sqrt.grid(row=1, column=1)
